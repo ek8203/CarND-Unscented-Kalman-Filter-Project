23b50a001080bbc471477887a54b0a4d1700fa24
ek8203 <efraim.kropp@flex.com>
2018-01-28 20:33:03 -0500

    removed conversion methods

--- src/tools.h
+++ src/tools.h
@@ -23,13 +23,6 @@ public:
   * A helper method to calculate RMSE.
   */
   VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
-
-  /*
-   * A helper methods to convert switch between Cartesian and Polar coordinates
-   */
-  VectorXd CartesianToPolar(const VectorXd& x_in);
-  VectorXd PolarToCartesian(const VectorXd& x_in);
-
 };
 
 #endif /* TOOLS_H_ */
